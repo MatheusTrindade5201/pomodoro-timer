1f93133a75aeb462da7730e0fcc859a4c1817c19
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-19 17:46:27 -0300

    feat: creating color pattern for both dark and light theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'dark',
+    settings:{
+        primary: '#020D21',
+        secondary: '#07223A',
+        primary_text: '#FDFEFE',
+        secondary_text: '#4E8193',
+    }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'light',
+    settings:{
+        primary: '#D6E4FF',
+        secondary: '#0A4977',
+        primary_text: '#F6FEFA',
+        secondary_text: '#07223A',
+    }
+}
