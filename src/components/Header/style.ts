c326f4be547ee24338f34fc93f45396894edcc22
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-19 18:16:21 -0300

    feat: passing colors through props

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,5 +1,16 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const HeaderContainer = styled.header`
+`
 
+export const Logo = styled.h1`
+    display: flex;
+    flex-direction:column;
+    align-items: flex-start;
+    font-size: 1.5rem;
+    color: ${props => props.theme.settings.primary_text};
+
+    P{
+        font-size: 1rem
+    }
 `
