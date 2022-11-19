45754bc65af3e8ef5ffcb797219c46136350676d
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-19 18:17:16 -0300

    feat: passing theme through ThemeProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
+import { ThemeProvider } from "styled-components"
 import Header from "./components/Header"
 import GlobalStyle from "./styles/global"
+import dark from "./styles/themes/dark"
+import light from "./styles/themes/light"
+
 
 function App() {
+
+  const [theme, setTheme] = useState(light)
+
   return (
     <div className="App">
-      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Header />
+        <GlobalStyle />
+      </ThemeProvider>
     </div>
   )
 }
