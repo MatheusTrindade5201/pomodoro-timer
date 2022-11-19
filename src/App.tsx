a1e5db0db86b285ba018458b65914841adb2aa7b
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-19 19:37:06 -0300

    fix: state types added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
-import { ThemeProvider } from "styled-components"
+import { DefaultTheme, ThemeProvider } from "styled-components"
 import Header from "./components/Header"
+import { MyContext } from "./context/myContext"
 import GlobalStyle from "./styles/global"
 import dark from "./styles/themes/dark"
 import light from "./styles/themes/light"
@@ -8,14 +9,16 @@ import light from "./styles/themes/light"
 
 function App() {
 
-  const [theme, setTheme] = useState(light)
+  const [theme , setTheme] = useState<DefaultTheme>(light)
 
   return (
     <div className="App">
+    <MyContext.Provider value={{theme, setTheme}}>
       <ThemeProvider theme={theme}>
         <Header />
         <GlobalStyle />
       </ThemeProvider>
+    </MyContext.Provider>
     </div>
   )
 }
