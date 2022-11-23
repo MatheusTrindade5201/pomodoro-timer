ac35a8ebe342e5de95c012a393d05d4ad9a2f57c
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-22 21:36:27 -0300

    feat: adding state for the times into the context

--- src/context/myContext.tsx
+++ src/context/myContext.tsx
@@ -5,13 +5,17 @@ export type GlobalContent = {
     theme: DefaultTheme,
     setTheme: (c:DefaultTheme) => void,
     open: boolean,
-    setOpen: (c:boolean) => void
+    setOpen: (c:boolean) => void,
+    times: Array<number>,
+    setTimes: (c:boolean) => void,
 }
 
 export const MyContext = createContext<GlobalContent>({
     theme: light,
     setTheme: () => {},
     open: false,
-    setOpen: () => {}
+    setOpen: () => {},
+    times: [25, 5, 10],
+    setTimes: () => {}
 });
 
