4c2e96594279a5f5213c98035466777af9fdfb21
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-24 21:24:58 -0300

    feat: timer style

--- src/components/Timer/style.ts
+++ src/components/Timer/style.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
 export const TimerContainer = styled.div`
-
+    background-color: #fff;
+    font-size: 5rem;
+    padding: .5rem;
+    border: 8px ${porps => porps.theme.settings.secondary};
+    border-style: double;
+    border-radius: 8px
 `
