14443d56fa0794b22a6e4607d50091a89783cc62
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-11-27 14:35:00 -0300

    feat: converting the value passed to the timer from minutes to seconds

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -5,9 +5,22 @@ export interface timerValues{
 }
 
 const Timer = (props:timerValues) => {
+
+    const timer = props.time*60;
+    const minute = Math.floor(timer/60);
+    const seconds = timer % 60;
+    const [minuteSetOfTen, minuteUnity] = String(minute).padStart(2,'0');
+    const [secondsSetOfTen, secondsUnity] = String(seconds).padStart(2,'0');
+
+    
+    
     return (
         <TimerContainer>
-            <span>{props.time}</span>
+            <span>{minuteSetOfTen}</span>
+            <span>{minuteUnity}</span>
+            <span>:</span>
+            <span>{secondsSetOfTen}</span>
+            <span>{secondsUnity}</span>
         </TimerContainer>
     )
 }
